4beb76c52de0c5f1d1b286dd998a066fa740bd6e
Raul Muniz <32320449+RaulMunizJr@users.noreply.github.com>
2020-05-16 18:39:41 -0500

    React prjt - blog

---
+++ my-blog/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+      <Route path="/" component={HomePage} exact /> {/*Route accepts two props, <Path Component> */}
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ my-blog/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HomePage = () => (
+    <>      {/* Same as React.Fragment */}
+    <h1>Welcome to my Blog!</h1>
+    <p>
+        Paragraph 1
+    </p>
+    <p>
+        Paragraph 2
+    </p>
+    </>
+);
+
+export default HomePage;
